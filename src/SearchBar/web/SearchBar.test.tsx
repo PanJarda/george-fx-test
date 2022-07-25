d1128620f68171af7fd53611f5970d5098b7cbef
Jarda Pernik <jaroslav.pernica9@gmail.com>
2022-07-25 04:04:18 +0200

    add SearchBarTest

---
+++ src/SearchBar/web/SearchBar.test.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import SearchBar from "./SearchBar";
+import createSetQueryAction from "../shared/redux/createSetQueryAction";
+
+const mockDispatch = jest.fn();
+
+const testQuery = "test";
+
+jest.mock("react-redux", () => ({
+	useDispatch: () => mockDispatch,
+	useSelector: () => "",
+}));
+
+const appPrefix = "SEARCH_BAR";
+
+describe("SearchBar", () => {
+	test("It should mount", async () => {
+		const user = userEvent.setup();
+		render(<SearchBar appPrefix={appPrefix} />);
+
+		const input = screen.getByRole("textbox");
+
+		expect(input).toHaveValue("");
+
+		await user.type(input, testQuery);
+
+		expect(mockDispatch).toHaveBeenCalledWith(
+			createSetQueryAction(appPrefix)("t")
+		);
+		expect(mockDispatch).toHaveBeenCalledWith(
+			createSetQueryAction(appPrefix)("e")
+		);
+		expect(mockDispatch).toHaveBeenCalledWith(
+			createSetQueryAction(appPrefix)("s")
+		);
+		expect(mockDispatch).toHaveBeenCalledWith(
+			createSetQueryAction(appPrefix)("t")
+		);
+	});
+});
